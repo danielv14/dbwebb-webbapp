3ebfa3255f5e9afc150615de3b7f0b147304b253
Daniel v <daniel.vernberg@gmail.com>
2015-11-12 10:11:37 +0100

    javascript kod för orientation testet

--- kmom02/me2/js/main.js
+++ kmom02/me2/js/main.js
@@ -8,4 +8,32 @@
     // Add shared panel as a panel.
     $("#menu").panel().enhanceWithin();
 
+    /**
+     * Check the orientation and size of the screen.
+     *
+     * @return void
+     */
+    window.checkSizeAndOrientation = function() {
+        var width = window.screen.width,
+            height = window.screen.height,
+            isPortrait = width < height,
+            text = "",
+            element = document.getElementById("orientationContent");
+
+        text  = "<p>Skärmens storlek är (w x h):<br>" + width + " x " + height;
+        text += "<p>Orienteringen är : " + (isPortrait ? "Porträtt" : "Landskap");
+
+        element.innerHTML = text;
+    };
+
+    /**
+     * Add eventhandler for the onresize event.
+     */
+    window.onresize = function() {
+        window.checkSizeAndOrientation();
+    };
+    // Call the function one to write out current orientation
+    window.checkSizeAndOrientation();
+
+
 })();
