d723ec642283691abadaa373f567ea477d176283
Daniel v <daniel.vernberg@gmail.com>
2015-11-25 10:53:03 +0100

    bilderna kommer fram i flödet nu

--- kmom03/me3/js/main.js
+++ kmom03/me3/js/main.js
@@ -206,84 +206,56 @@
     getjson.addEventListener("click", window.getMarvinQuoteByGetJSON);
 
 
+    /*
+    * Code for Instagram here
+    */
 
-    $(document).on('pagebeforeshow', '#af-totalt', function(event, data){
 
-        $.ajax({
-            url: "../ajax/arbetsformedlingen/soklista_lan.json",
-            dataType: "json",
+    /**
+    * Cache the JSON objekt.
+    */
+   var soklistaLan = null;
+   $(document).on('pagebeforeshow', '#instagram-lista', function(/* event, data */){
 
-            success: function (data) {
-                soklistaLan = data;
+       if (soklistaLan !== null) {
+           window.updateInstramList();
+           return;
+       }
 
-                var totalFree = document.getElementById("totalt_antal_ledigajobb");
-                var totalAds = document.getElementById("totalt_antal_platsannonser");
+       $.ajax({
+           url: "../me3/instagram/tag-dbwebb.json",
+           dataType: "json",
 
-                totalFree.innerHTML = soklistaLan.soklista.totalt_antal_ledigajobb;
-                totalAds.innerHTML = soklistaLan.soklista.totalt_antal_platsannonser;
-            },
+           success: function (data) {
+               soklistaLan = data;
+               window.updateInstramList();
+           },
 
-            error: function (/* request, error */) {
-                console.log('Network error has occurred please try again!');
-            }
-        });
-    });
+           error: function (/* request, error */) {
+               console.log('Network error has occurred please try again!');
+           }
+       });
+   });
 
-     /**
-     * Update the page showing AF total.
-     */
-    window.updateAFTotal = function() {
-        var totalFree = document.getElementById("totalt_antal_ledigajobb");
-        var totalAds = document.getElementById("totalt_antal_platsannonser");
-
-        totalFree.innerHTML = soklistaLan.soklista.totalt_antal_ledigajobb;
-        totalAds.innerHTML = soklistaLan.soklista.totalt_antal_platsannonser;
-    };
 
      /**
-     * Cache the JSON objekt.
-     */
-    var soklistaLan = null;
-    $(document).on('pagebeforeshow', '#af-totalt', function(/* event, data */){
-
-        if (soklistaLan !== null) {
-            window.updateAFTotal();
-            return;
-        }
-
-        $.ajax({
-            url: "../ajax/arbetsformedlingen/soklista_lan.json",
-            dataType: "json",
-
-            success: function (data) {
-                soklistaLan = data;
-                window.updateAFTotal();
-            },
-
-            error: function (/* request, error */) {
-                console.log('Network error has occurred please try again!');
-            }
-        });
-    });
-
-     /**
-     * Get JSON, if not already available and update AF list,
+     * Get JSON, if not already available and update Instagram list,
      * before loading page.
      */
-    $(document).on('pagebeforeshow', '#af-lista', function(/* event, data */){
+    $(document).on('pagebeforeshow', '#instagram-lista', function(/* event, data */){
 
         if (soklistaLan !== null) {
-          window.updateAFList();
+          window.updateInstagramList();
           return;
         }
 
         $.ajax({
-            url: "../ajax/arbetsformedlingen/soklista_lan.json",
+            url: "../me3/instagram/tag-dbwebb.json",
             dataType: "json",
 
             success: function (data) {
                 soklistaLan = data;
-                window.updateAFList();
+                window.updateInstagramList();
             },
 
             error: function (/* request, error */) {
@@ -293,81 +265,23 @@
     });
 
      /**
-     * Update AP list with free jobs for each county.
+     * Update Instagram list with pictures.
      */
-    window.updateAFList = function() {
-        var list = document.getElementById("af-listview");
+    window.updateInstagramList = function() {
+        var list = document.getElementById("instagram-listview");
         var html="";
 
-        soklistaLan.soklista.sokdata.forEach(function(row) {
-            //html += "<li>" + row.namn + " (" + row.antal_ledigajobb + " lediga jobb)</li>";
-            html += "<li><a href='#af-lista-" + row.id + "'>" + row.namn + " (" + row.antal_ledigajobb + " lediga jobb)</a></li>";
+        soklistaLan.data.forEach(function(row) {
+            //html += "<li><a href='#instagram-lista-" + row.id + "'>" + row.namn + " (" + row.antal_ledigajobb + " lediga jobb)</a></li>";
+            html += '<img src="' + row.images.standard_resolution.url + '" alt="instagram-bild">';
+            //html += '<li><p>Länk till bilden: ' + row.images.thumbnail.url + '</p></li>';
         });
 
         list.innerHTML = html;
 
-        $('#af-listview').listview('refresh');
+        $('#instagram-listview').listview('refresh');
     };
 
-     /**
-     * Display subpage, expect that JSON is already loaded.
-     */
-    var afSubPageId = null;
-
-    $(document).on('pagebeforeshow', '#af-sida', function(/*event, data*/){
-
-       window.updateAFSubPage(afSubPageId);
-
-    });
-
-     /**
-     * Update subpage with details from specified county.
-     */
-    window.updateAFSubPage = function(pageId) {
-        var element = document.getElementById("af-undersida");
-        var html="Specified page id not found.";
-
-        soklistaLan.soklista.sokdata.forEach(function(row) {
-            if (row.id == pageId) {
-                html = "<h1>" + row.namn + "</h1><p>Det finns " + row.antal_ledigajobb + " lediga jobb och " + row.antal_platsannonser + " platsannonser.</p>";
-                return;
-            }
-        });
-
-        element.innerHTML = html;
-    };
-
-     /**
-     * Intercept change of page and implement routing.
-     */
-    $("body").on( "pagecontainerbeforechange", function( event, ui ) {
-        var to = ui.toPage;
-        var from = ui.options.fromPage;
-
-        // If not a valid pageid
-        if (typeof to  === 'string') {
-            var url = $.mobile.path.parseUrl(to);
-            var toSubPage;
-
-            to = url.hash || '#' + url.pathname.substring(1);
-
-            if (from) {
-                from = '#' + from.attr('id');
-            }
-     
-            var length = "#af-lista-".length;
-            toSubPage = to.substring(0, length);
-
-            if (from === '#af-lista' && toSubPage === '#af-lista-') {
-                event.preventDefault();
-                event.stopPropagation();
-
-                afSubPageId = to.substring(length);
-                console.log("Subpageid = " + afSubPageId);
-                $(":mobile-pagecontainer").pagecontainer("change", "#af-sida", { foo: "Hello World!" });
-            }
-        }
-    });
 
 
 
