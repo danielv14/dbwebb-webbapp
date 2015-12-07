69eadbffd3c55404c2ab4da58afd3b1d8fb5a3df
Daniel v <daniel.vernberg@gmail.com>
2015-12-07 12:36:35 +0100

    tog bort js-kod som inte används

--- kmom05/pizza/js/main.js
+++ kmom05/pizza/js/main.js
@@ -8,134 +8,7 @@
     // Add shared panel as a panel.
     $("#menu").panel().enhanceWithin();
 
-    /**
-     * Check the orientation and size of the screen.
-     *
-     * @return void
-     */
-    window.checkSizeAndOrientation = function() {
-        var width = window.screen.width,
-            height = window.screen.height,
-            availwidth = window.screen.availWidth,
-            availheigth = window.screen.availHeight,
-            availleft = window.screen.availLeft,
-            colordepth = window.screen.colorDepth,
-            pixeldepth = window.screen.pixelDepth,
-            isPortrait = width < height,
-            text = "",
-            element = document.getElementById("orientationContent");
 
-        text  = "<p>Skärmens storlek är (w x h):<br>" + width + " x " + height;
-        text += "<p>Orienteringen är : " + (isPortrait ? "Porträtt" : "Landskap");
-        text += "<p>AvailWidth är : " + availwidth + " pixlar";
-        text += "<p>AvailHeight är : " + availheigth + " pixlar";
-        text += "<p>AvailLeft är : " + availleft + " pixlar";
-        text += "<p>ColorDepth är : " + colordepth;
-        text += "<p>PixelDepth är : " + pixeldepth;
-
-
-        element.innerHTML = text;
-    };
-
-    /**
-     * Add eventhandler for the onresize event.
-     */
-    window.onresize = function() {
-        window.checkSizeAndOrientation();
-    };
-    // Call the function one to write out current orientation
-    window.checkSizeAndOrientation();
-
-    /**
-     * Add eventhandler for the click event.
-     */
-    $(window).on("click", function() {
-        var swipeElement = document.getElementById("swipeContent");
-        swipeElement.innerHTML += "click, ";
-    });
-
-    /**
-     * Add eventhandler for the swipe event.
-     */
-    $(window).on("swipe", function() {
-        var swipeElement = document.getElementById("swipeContent");
-        swipeElement.innerHTML += "swipe, ";
-    });
-
-    /**
-     * Add eventhandler for the swiperight event.
-     */
-    $(window).on("swiperight", function() {
-        var swipeElement = document.getElementById("swipeContent");
-        swipeElement.innerHTML += "swiperight, ";
-    });
-
-    /**
-     * Add eventhandler for the swipeleft event.
-     */
-    $(window).on("swipeleft", function() {
-        var swipeElement = document.getElementById("swipeContent");
-        swipeElement.innerHTML += "swipeleft, ";
-    });
-
-    /**
-     * Add eventhandler for the tap event.
-     */
-    $(window).on("tap", function() {
-        var swipeElement = document.getElementById("swipeContent");
-        swipeElement.innerHTML += "tap, ";
-    });
-
-    /**
-     * Add eventhandler for the taphold event.
-     */
-    $(window).on("taphold", function() {
-        var swipeElement = document.getElementById("swipeContent");
-        swipeElement.innerHTML += "taphold, ";
-    });
-
-    /**
-     * Add eventhandler for the touch event.
-     */
-    $(window).on("touchstart", function() {
-        var touchElement = document.getElementById("touchContent");
-        touchElement.innerHTML += "touchstart, ";
-    });
-
-    /**
-     * Add eventhandler for the touch event.
-     */
-    $(window).on("touchend", function() {
-        var touchElement = document.getElementById("touchContent");
-        touchElement.innerHTML += "touchend, ";
-    });
-
-    // Init canvas
-    var canvas = document.getElementById("draw");
-    var ctx = canvas.getContext("2d");
-
-    /**
-     * Add eventhandler for click on canvas.
-     */
-    $('#draw').on("click", function(event) {
-        ctx.fillStyle = "#FF0000";
-        ctx.fillRect(event.offsetX-5, event.offsetY-5, 10, 10);
-    });
-
-    /**
-     * Add eventhandler for click on canvas.
-     */
-     $('#draw').on("touchmove", function(event) {
-        event.preventDefault();
-        //console.log(event);
-        var offsetX = canvas.offsetLeft;
-        var offsetY = canvas.offsetTop;
-        var x = event.originalEvent.changedTouches[ 0 ].clientX - offsetX;
-        var y = event.originalEvent.changedTouches[ 0 ].clientY - offsetY;
-        //console.log("pos " + x + "x" + y);
-        ctx.fillStyle = "#00FF00";
-        ctx.fillRect(x-5, y-5, 10, 10);
-    });
 
     /**
     * Add eventhandler for the navigate back event.
@@ -145,14 +18,6 @@
         return false;
     });
 
-    /**
-     * Add eventhandler for the clickevent as part of own test
-     */
-    $(window).on("click", function() {
-
-        $("#clickToby").toggleClass("round");
-
-    });
 
 
      /**
@@ -270,7 +135,7 @@
      * Get JSON, if not already available and update AF list,
      * before loading page.
      */
-    $(document).on('pagebeforeshow', '#meny', function(/* event, data */){
+    $(document).on('pagebeforeshow', '#af-lista', function(/* event, data */){
 
         if (soklistaLan !== null) {
           window.updateAFList();
@@ -301,7 +166,7 @@
 
         soklistaLan.soklista.sokdata.forEach(function(row) {
             //html += "<li>" + row.namn + " (" + row.antal_ledigajobb + " lediga jobb)</li>";
-            html += "<li><a href='#meny-" + row.id + "'>" + row.namn + " (" + row.antal_ledigajobb + " lediga jobb)</a></li>";
+            html += "<li><a href='#af-lista-" + row.id + "'>" + row.namn + " (" + row.antal_ledigajobb + " lediga jobb)</a></li>";
         });
 
         list.innerHTML = html;
@@ -355,10 +220,10 @@
                 from = '#' + from.attr('id');
             }
 
-            var length = "#meny-".length;
+            var length = "#af-lista-".length;
             toSubPage = to.substring(0, length);
 
-            if (from === '#meny' && toSubPage === '#meny-') {
+            if (from === '#af-lista' && toSubPage === '#af-lista-') {
                 event.preventDefault();
                 event.stopPropagation();
 
@@ -373,7 +238,7 @@
     // Pizzeria Code here
 
 
-    // function for google maps on contact page
+    // function for google maps on contact
     function initMap() {
       var myLatLng = {lat: 63.827894, lng: 20.256578};
 
