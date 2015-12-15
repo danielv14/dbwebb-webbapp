ad2f47c4e997280e737d20b85b071ce48390d464
Daniel v <daniel.vernberg@gmail.com>
2015-12-15 14:17:34 +0100

    installerade spalshscreen plugin

--- kmom06/cordova/pizza/platforms/android/assets/www/cordova_plugins.js
+++ kmom06/cordova/pizza/platforms/android/assets/www/cordova_plugins.js
@@ -5,12 +5,21 @@ module.exports = [
         "id": "cordova-plugin-whitelist.whitelist",
         "pluginId": "cordova-plugin-whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.0"
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-splashscreen": "3.0.0"
 }
 // BOTTOM OF METADATA
 });
