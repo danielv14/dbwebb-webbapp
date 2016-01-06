22de849646542d5f78f5847972267a59f69d7414
Daniel v <daniel.vernberg@gmail.com>
2016-01-06 11:30:31 +0100

    valideringsfel

--- kmom10/proj/www/js/app.js
+++ kmom10/proj/www/js/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function()    {
 
   // get bootstrap navbar to collapse on click
-  $(document).on('click',function(){
+  $(document).on('click', function(){
     $('.collapse').collapse('hide');
   })
 
